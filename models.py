acec4399eb23685b5e1f009e957081e6b02f21bb
Renasha Mishra <56019555+Renasha@users.noreply.github.com>
2020-10-03 04:27:11 +0530

    Update models.py

--- models.py
+++ models.py
@@ -31,7 +31,7 @@ for char_ID in char_dict:
 	char_vec.append(char_dict[char_ID])
 
 char_vec = np.asarray(char_vec)
-kmeans = klearn.cluster.KMeans(n_clusters=8, *, init='k-means++', n_init=10, max_iter=300, tol=0.0001, precompute_distances='deprecated', verbose=0, random_state=None, copy_x=True, n_jobs='deprecated', algorithm='auto').fit(char_vec)
+kmeans = sklearn.cluster.KMeans(n_clusters=8, *, init='k-means++', n_init=10, max_iter=300, tol=0.0001, precompute_distances='deprecated', verbose=0, random_state=None, copy_x=True, n_jobs='deprecated', algorithm='auto').fit(char_vec)
 
 for mov_ID in Pre_process.movieToVec:
 	movie_vec.append(char_dict[char_ID])
